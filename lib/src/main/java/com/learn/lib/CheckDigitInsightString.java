e00526f37ab51bbefaaee6d52c1136a3432fb0b3
Tejas <agravat.tejas.1997@gmail.com>
2020-08-24 09:26:42 +0530

    Check Digigit Program

--- lib/src/main/java/com/learn/lib/CheckDigitInsightString.java
+++ lib/src/main/java/com/learn/lib/CheckDigitInsightString.java
@@ -4,7 +4,30 @@ public class CheckDigitInsightString {
 
     public static void main(String[] arg)
     {
+        String s ="1Tejas";
+        if (checkDigit(s))
+        {
+            System.out.println("Yes Digit");
+        }
+        else
+            System.out.println("No Digit");
 
+    }
+    private static boolean checkDigit(String string)
+    {
+        if (string!=null)
+        {
+            for (char c:string.toCharArray())
+            {
+                if (c>='0' && c<='9')
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+        }
+        return false;
     }
 
 }
